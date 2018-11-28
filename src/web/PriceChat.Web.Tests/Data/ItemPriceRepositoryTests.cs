962c481ea52b65d1be4a8eeee4c3d7635eb0410a
Mark Davies <markdavies550@gmail.com>
2018-11-28 21:40:37 +0000

    Finishing repository tests

--- src/web/PriceChat.Web.Tests/Data/ItemPriceRepositoryTests.cs
+++ src/web/PriceChat.Web.Tests/Data/ItemPriceRepositoryTests.cs
@@ -24,7 +24,7 @@ namespace PriceChat.Web.Tests.Data
             [Fact]
             public async Task WhenThereAreItemsInTheTable_ItemsAreReturned()
             {
-                var item = CreateItemPrice();
+                var item = CreateItemPrice("b07");
                 await _fixture.Repository.Add(item);
 
                 var results = await _fixture.Repository.GetAll();
@@ -32,12 +32,50 @@ namespace PriceChat.Web.Tests.Data
                 Assert.Equal(item.PartitionKey, results[0].PartitionKey);
                 Assert.Equal(item.RowKey, results[0].RowKey);
             }
+        }
+        
+        public class GetByPartitionKeyTests : IClassFixture<ItemPriceRepositoryFixtureFactory>, IDisposable
+        {
+            private readonly ItemPriceRepositoryFixture _fixture;
 
-            private ItemPrice CreateItemPrice() => new ItemPrice
+            public GetByPartitionKeyTests(ItemPriceRepositoryFixtureFactory fixtureFactory) 
+                => _fixture = fixtureFactory.Build();
+
+            public void Dispose() 
+                => _fixture.Dispose();
+
+            [Fact]
+            public async Task WhenThereAreNoItemsInTheTable_EmptyCollectionIsReturned()
+                => Assert.Empty(await _fixture.Repository.ByPartitionKey(string.Empty));
+
+            [Fact]
+            public async Task WhenThereAreNoItemsWithTheSearchedPartitionKey_ThenEmptyCollectionIsReturned()
             {
-                PartitionKey = "b07",
-                RowKey = $"{Guid.NewGuid():N}"
-            };
-        }        
+                var item = CreateItemPrice("b07");
+                await _fixture.Repository.Add(item);
+
+                var results = await _fixture.Repository.ByPartitionKey("0");
+                Assert.Empty(results);
+            }
+            
+            [Fact]
+            public async Task WhenThereAreItemsWithSearchedPartitionKey_ThenItemPriceIsReturned()
+            {
+                const string partitionKey = "b07";
+                var item = CreateItemPrice(partitionKey);
+                await _fixture.Repository.Add(item);
+
+                var results = await _fixture.Repository.ByPartitionKey(partitionKey);
+                Assert.NotEmpty(results);
+                Assert.Equal(item.PartitionKey, results[0].PartitionKey);
+                Assert.Equal(item.RowKey, results[0].RowKey);
+            }
+        }
+
+        private static ItemPrice CreateItemPrice(string partitionKey) => new ItemPrice
+        {
+            PartitionKey = partitionKey,
+            RowKey = $"{Guid.NewGuid():N}"
+        };
     }
 }
