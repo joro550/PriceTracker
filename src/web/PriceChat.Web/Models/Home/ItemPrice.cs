8ba4056f7abaa05e799a12acacf36e04d3eb9f1e
Mark Davies <markdavies550@gmail.com>
2018-12-02 17:07:58 +0000

    Fixing ordering on price pages

--- src/web/PriceChat.Web/Models/Home/ItemPrice.cs
+++ src/web/PriceChat.Web/Models/Home/ItemPrice.cs
@@ -7,5 +7,6 @@ namespace PriceChat.Web.Models.Home
         public string PartitionKey { get; set; }
         public string Price { get; set; }
         public DateTime Timestamp { get; set; }
+        public DateTime PriceDate { get; set; }
     }
 }
