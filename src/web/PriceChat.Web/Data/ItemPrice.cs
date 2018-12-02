d230e91443af75f5634d992fbdc8b9ca6f954e52
Mark Davies <markdavies550@gmail.com>
2018-12-02 13:54:28 +0000

    Changing to my date time stamp

--- src/web/PriceChat.Web/Data/ItemPrice.cs
+++ src/web/PriceChat.Web/Data/ItemPrice.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.WindowsAzure.Storage.Table;
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace PriceChat.Web.Data
 {
     public class ItemPrice : TableEntity
     {
         public string Price { get; set; }
+        public DateTime PriceDate { get; set; }
     }
 }
--- src/web/PriceChat.Web/Models/ChartData.cs
+++ src/web/PriceChat.Web/Models/ChartData.cs
@@ -12,13 +12,13 @@ namespace PriceChat.Web.Models
         {
             var chartData = new ChartData
             {
-                Labels = prices.Select(p => p.Timestamp.ToString("yyyy-M-d")).Distinct().ToList()
+                Labels = prices.Select(p => p.PriceDate.ToString("yyyy-M-d")).Distinct().OrderBy(x => x).ToList()
             };
 
             foreach (var groupedPrice in prices.GroupBy(price => price.PartitionKey))
             {
                 var dataSet = new ChatDataSets{Label = groupedPrice.Key};
-                foreach (var itemPrice in groupedPrice.OrderBy(price => price.Timestamp))
+                foreach (var itemPrice in groupedPrice.OrderBy(price => price.PriceDate))
                     dataSet.Data.Add(string.IsNullOrWhiteSpace(itemPrice.Price) ? "" : itemPrice.Price.Remove(0, 1));
 
                 chartData.DataSets.Add(dataSet);
